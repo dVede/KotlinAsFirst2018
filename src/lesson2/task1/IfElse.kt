635af3d715a8a803c544029a9dce8b032d4ace2a
dVede <dvede123@yandex.ru>
2018-09-11 15:06:03 +0300

    SIMLPLE IFELSE LOGICAL DONE

--- src/lesson2/task1/IfElse.kt
+++ src/lesson2/task1/IfElse.kt
@@ -2,6 +2,7 @@
 package lesson2.task1
 
 import lesson1.task1.discriminant
+import kotlin.math.PI
 import kotlin.math.max
 import kotlin.math.sqrt
 
@@ -62,7 +63,10 @@ fun minBiRoot(a: Double, b: Double, c: Double): Double {
  * Мой возраст. Для заданного 0 < n < 200, рассматриваемого как возраст человека,
  * вернуть строку вида: «21 год», «32 года», «12 лет».
  */
-fun ageDescription(age: Int): String = TODO()
+fun ageDescription(age: Int): String { return when{
+    age % 10 == 1 && age % 100 != 11 -> "$age год"
+    age % 10 in 2..4 && age % 100 !in 12..14 -> "$age года"
+    else -> "$age лет" } }
 
 /**
  * Простая
@@ -73,7 +77,16 @@ fun ageDescription(age: Int): String = TODO()
  */
 fun timeForHalfWay(t1: Double, v1: Double,
                    t2: Double, v2: Double,
-                   t3: Double, v3: Double): Double = TODO()
+                   t3: Double, v3: Double): Double {
+    val s1 = t1 * v1
+    val s2 = t2 * v2
+    val s3 = t3 * v3
+    val s = (s1 + s2 + s3) / 2
+    return when{
+        s1 > s -> s / v1
+        s2 + s1 > s -> (s - s1) / v2 + t1
+        else -> (s - s1 - s2) / v3 + t1 + t2
+}}
 
 /**
  * Простая
@@ -86,7 +99,13 @@ fun timeForHalfWay(t1: Double, v1: Double,
  */
 fun whichRookThreatens(kingX: Int, kingY: Int,
                        rookX1: Int, rookY1: Int,
-                       rookX2: Int, rookY2: Int): Int = TODO()
+                       rookX2: Int, rookY2: Int): Int {
+    return if (((kingX == rookX1) || (kingY == rookY1)) && ((kingX == rookX2) || (kingY == rookY2))) 3
+    else if ((kingX == rookX1) || (kingY == rookY1)) 1
+    else if ((kingX == rookX2) || (kingY == rookY2)) 2
+    else 0
+}
+
 
 /**
  * Простая
@@ -100,7 +119,13 @@ fun whichRookThreatens(kingX: Int, kingY: Int,
  */
 fun rookOrBishopThreatens(kingX: Int, kingY: Int,
                           rookX: Int, rookY: Int,
-                          bishopX: Int, bishopY: Int): Int = TODO()
+                          bishopX: Int, bishopY: Int): Int {
+    return if ((Math.abs(kingX - bishopX) == Math.abs(kingY - bishopY))
+            && ((kingX == rookX) || (kingY == rookY))) 3
+    else if (Math.abs(kingX - bishopX) == Math.abs(kingY - bishopY)) 2
+    else if ((kingX == rookX) || (kingY == rookY)) 1
+    else 0
+}
 
 /**
  * Простая
@@ -110,7 +135,21 @@ fun rookOrBishopThreatens(kingX: Int, kingY: Int,
  * прямоугольным (вернуть 1) или тупоугольным (вернуть 2).
  * Если такой треугольник не существует, вернуть -1.
  */
-fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
+fun triangleKind(a: Double, b: Double, c: Double): Int {
+    val x = (a * a + b * b - c * c) / (2.0 * a * b)
+    val y = (a * a + c * c - b * b) / (2.0 * a * c)
+    val z = (c * c + b * b - a * a) / (2.0 * c * b)
+       return if (a + b < c || a + c < b || c + b < a) -1
+       else
+           return when {
+            x > 0 && y > 0 && z > 0 -> 0
+            x == 0.0 || y == 0.0 || z == 0.0 -> 1
+            x < 0 || y < 0 || z < 0 -> 2
+            else -> -1
+           }
+}
+
+
 
 /**
  * Средняя
@@ -120,4 +159,11 @@ fun triangleKind(a: Double, b: Double, c: Double): Int = TODO()
  * Найти длину пересечения отрезков AB и CD.
  * Если пересечения нет, вернуть -1.
  */
-fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int = TODO()
+fun segmentLength(a: Int, b: Int, c: Int, d: Int): Int {
+    val x = max(a, c)
+    val y = Math.min(b, d)
+    return when {
+        y >= x -> y - x
+        else -> -1
+    }
+}
--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -17,7 +17,9 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    return if (number / 1000 + (number / 100) % 10 == (number / 10) % 10 + number % 10) true
+    else false}
 
 /**
  * Простая
@@ -26,7 +28,9 @@ fun isNumberHappy(number: Int): Boolean = TODO()
  * Определить, угрожают ли они друг другу. Вернуть true, если угрожают.
  * Считать, что ферзи не могут загораживать друг друга.
  */
-fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
+fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean
+{return if ((x1 == x2)||(y1 == y2)||((x1 - x2) * (x1 - x2) == (y1 - y2)*(y1 - y2))) true
+else false}
 
 
 /**
@@ -35,7 +39,15 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int {
+
+    return when{((((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))) && month == 2 -> 29
+        (((month % 2 == 0) && (month >= 8)) || ((month % 2 == 1) && (month <= 8))) -> 31
+        (((month % 2 == 1) && (month >= 8)) || ((month % 2 == 0) && (month <= 8) && (month != 2))) -> 30
+        else -> 28
+    }
+}
+
 
 /**
  * Средняя
@@ -45,7 +57,12 @@ fun daysInMonth(month: Int, year: Int): Int = TODO()
  * Вернуть true, если утверждение верно
  */
 fun circleInside(x1: Double, y1: Double, r1: Double,
-                 x2: Double, y2: Double, r2: Double): Boolean = TODO()
+                 x2: Double, y2: Double, r2: Double): Boolean{
+    return when {((Math.sqrt(sqr(x1 - x2) + sqr(y1 - y2)) + r1) <= r2) -> true
+    else -> false
+}
+}
+
 
 /**
  * Средняя
@@ -56,4 +73,10 @@ fun circleInside(x1: Double, y1: Double, r1: Double,
  * кирпич 4 х 4 х 4 пройдёт через отверстие 4 х 4.
  * Вернуть true, если кирпич пройдёт
  */
-fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean = TODO()
+fun brickPasses(a: Int, b: Int, c: Int, r: Int, s: Int): Boolean {
+    return when {
+        (((a <= r) && (b <= s)) || ((a <= s) && (b <= r))) -> true
+        (((a <= r) && (c <= s)) || ((a <= s) && (c <= r))) -> true
+        (((c <= r) && (b <= s)) || ((c <= s) && (b <= r))) -> true
+        else -> false
+}}
